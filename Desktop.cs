e011c17829c2f002250f1c715381921637865d4c
B1gtac <165765943+NotB1gtac@users.noreply.github.com>
2025-07-13 19:30:29 +0200

    updated Version of AudioPlayer on Desktop

--- Desktop.cs
+++ Desktop.cs
@@ -37,7 +37,7 @@ namespace B1_Apps
 				(s, e) => new B1_Apps.Apps.Game.Form1().Show()));
 			tilePanel.Controls.Add(CreateTile("YT Downloader (2.0)", Properties.Resources.YTdownloader,
 				(s, e) => new B1_Apps.Apps.YTdownloader.YTdownloader().Show()));
-			tilePanel.Controls.Add(CreateTile("Audio Player (1.3)", Properties.Resources.AudioPlayer,
+			tilePanel.Controls.Add(CreateTile("Audio Player (2.1)", Properties.Resources.AudioPlayer,
 				(s, e) => new B1_Apps.Apps.AudioPlayer.AudioPlayer().Show()));
 			tilePanel.Controls.Add(CreateTile("Audio/Video Convert", Properties.Resources.Converter,
 				(s, e) => new B1_Apps.Apps.FormatConverter.FormatConverterForm().Show()));
